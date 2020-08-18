7dc3fbfe34980a88868649da980e04c94b474094
Donghee Han <hdh7485@gmail.com>
2020-08-19 00:28:33 +0900

    Added odrive part in the flying_car.ino

--- flying_car.ino
+++ flying_car.ino
@@ -1,13 +1,26 @@
 #include <Dynamixel2Arduino.h>
 #include <math.h>
 #include <SBUS.h>
+#include <ODriveArduino.h>
 
 #define DEBUG_SERIAL  Serial
 #define DXL_SERIAL    Serial1
 #define SBUS_SERIAL   Serial2
+#define ODRIVE_SERIAL Serial3
 
 #define DEBUG_SERIAL_BAUDRATE 115200
 #define DXL_SERIAL_BAUDRATE 115200
+#define ODRIVE_SERIAL_BAUDRATE 115200
+
+// Printing with stream operator
+template<class T> inline Print& operator <<(Print &obj,     T arg) {
+  obj.print(arg);
+  return obj;
+}
+template<>        inline Print& operator <<(Print &obj, float arg) {
+  obj.print(arg, 4);
+  return obj;
+}
 
 const uint8_t DXL_DIR_PIN = 2; // DYNAMIXEL Shield DIR PIN
 
@@ -15,29 +28,30 @@ const uint8_t LEFT_DXL_ID = 1;
 const uint8_t RIGHT_DXL_ID = 2;
 const float   DXL_PROTOCOL_VERSION = 2.0;
 
-class AckermannGeometry{
-private:
-  const double  WHEEL_FRONT_WIDTH = 1.0; //m
-  const double  WHEEL_REAR_WIDTH = 1.0; //m
-  const double  WHEEL_VERTICAL_DISTANCE = 2.0; //m
-  const double  WHEEL_RADIUS = 0.127; //m
+class AckermannGeometry {
+  private:
+    const double  WHEEL_FRONT_WIDTH = 1.0; //m
+    const double  WHEEL_REAR_WIDTH = 1.0; //m
+    const double  WHEEL_VERTICAL_DISTANCE = 2.0; //m
+    const double  WHEEL_RADIUS = 0.127; //m
 
-public:
-  double left_steer_angle;
-  double right_steer_angle;
-  double left_rear_rpm;
-  double right_rear_rpm;
+  public:
+    double left_steer_angle;
+    double right_steer_angle;
+    double left_rear_rpm;
+    double right_rear_rpm;
 
-  void calculate(double steering_angle, double speed){  //steering_angle: radian, speed: m/s
-    double R = tan(steering_angle) / WHEEL_VERTICAL_DISTANCE;
-    left_steer_angle = atan2(WHEEL_VERTICAL_DISTANCE, (R - WHEEL_FRONT_WIDTH/2));
-    right_steer_angle = atan2(WHEEL_VERTICAL_DISTANCE, (R + WHEEL_FRONT_WIDTH/2));
-    //self.left_rear_rpm = 
-    //self.right_rear_rpm = 
-  }
+    void calculate(double steering_angle, double speed) { //steering_angle: radian, speed: m/s
+      double R = tan(steering_angle) / WHEEL_VERTICAL_DISTANCE;
+      left_steer_angle = atan2(WHEEL_VERTICAL_DISTANCE, (R - WHEEL_FRONT_WIDTH / 2));
+      right_steer_angle = atan2(WHEEL_VERTICAL_DISTANCE, (R + WHEEL_FRONT_WIDTH / 2));
+      //self.left_rear_rpm =
+      //self.right_rear_rpm =
+    }
 
 };
 
+ODriveArduino odrive(ODRIVE_SERIAL);
 Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
 SBUS x8r(SBUS_SERIAL);
 AckermannGeometry ackermann_geometry;
@@ -45,11 +59,28 @@ AckermannGeometry ackermann_geometry;
 float channels[16];
 bool failSafe;
 bool lostFrame;
+
 float target_steering_degree;
+double target_wheel_rpm;
+
+bool motor1_calibration_finish;
 
 void setup() {
+  ODRIVE_SERIAL.begin(ODRIVE_SERIAL_BAUDRATE);
+
   // Use UART port of DYNAMIXEL Shield to debug.
   DEBUG_SERIAL.begin(DEBUG_SERIAL_BAUDRATE);
+  DEBUG_SERIAL.println("ODriveArduino");
+  DEBUG_SERIAL.println("Setting parameters...");
+
+  // In this example we set the same parameters to both motors.
+  // You can of course set them different if you want.
+  // See the documentation or play around in odrivetool to see the available parameters
+  for (int axis = 0; axis < 2; ++axis) {
+    ODRIVE_SERIAL << "w axis" << axis << ".controller.config.vel_limit " << 22000.0f << '\n';
+    ODRIVE_SERIAL << "w axis" << axis << ".motor.config.current_lim " << 11.0f << '\n';
+    // This ends up writing something like "w axis0.motor.config.current_lim 10.0\n"
+  }
 
   dxl.begin(DXL_SERIAL_BAUDRATE);
   // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
@@ -71,15 +102,45 @@ void setup() {
 
 void loop() {
   if (x8r.readCal(&channels[0], &failSafe, &lostFrame)) {
-    target_steering_degree = *(channels+0) * 30.0 * M_PI/180;
+//    target_steering_degree = *(channels + 0) * 30.0 * M_PI / 180;
+    target_steering_degree = *(channels + 0) * -30.0;
     ackermann_geometry.calculate(target_steering_degree, 1);
-    dxl.setGoalPosition(LEFT_DXL_ID, target_steering_degree, UNIT_DEGREE);
-    dxl.setGoalPosition(RIGHT_DXL_ID, target_steering_degree, UNIT_DEGREE);
-    delay(1000);
-    // Print present position in degree value
-    DEBUG_SERIAL.print("Present Position(degree) : ");
-    DEBUG_SERIAL.println(dxl.getPresentPosition(LEFT_DXL_ID, UNIT_DEGREE));
-    DEBUG_SERIAL.println(dxl.getPresentPosition(RIGHT_DXL_ID, UNIT_DEGREE));
-    delay(1000);
+    target_wheel_rpm = (*(channels + 1) * 150) - 4.3;
   }
+  if (motor1_calibration_finish) {
+//    DEBUG_SERIAL.println("Executing test move");
+    odrive.SetVelocity(0, target_wheel_rpm);
+    dxl.setGoalPosition(LEFT_DXL_ID, target_steerinMg_degree, UNIT_DEGREE);
+    dxl.setGoalPosition(RIGHT_DXL_ID, target_steering_degree, UNIT_DEGREE);
+    DEBUG_SERIAL.println(target_steering_degree);
+//    DEBUG_SERIAL.println("Executing test move");
+  }
+//  dxl.setGoalPosition(LEFT_DXL_ID, target_steering_degree, UNIT_DEGREE);
+//  dxl.setGoalPosition(RIGHT_DXL_ID, target_steering_degree, UNIT_DEGREE);
+
+  if (DEBUG_SERIAL.available()) {
+    char c = DEBUG_SERIAL.read();
+
+    // Run calibration sequence
+    if (c == '0' || c == '1') {
+      int motornum = c - '0';
+      int requested_state;
+
+      requested_state = ODriveArduino::AXIS_STATE_MOTOR_CALIBRATION;
+      DEBUG_SERIAL << "Axis" << c << ": Requesting state " << requested_state << '\n';
+      odrive.run_state(motornum, requested_state, true);
+
+      requested_state = ODriveArduino::AXIS_STATE_ENCODER_OFFSET_CALIBRATION;
+      DEBUG_SERIAL << "Axis" << c << ": Requesting state " << requested_state << '\n';
+      odrive.run_state(motornum, requested_state, true);
+
+      requested_state = ODriveArduino::AXIS_STATE_CLOSED_LOOP_CONTROL;
+      DEBUG_SERIAL << "Axis" << c << ": Requesting state " << requested_state << '\n';
+      odrive.run_state(motornum, requested_state, false); // don't wait
+      delay(100);
+      motor1_calibration_finish = true;
+    }
+  }
+
+  delay(10);
 }
--- odrive_speed_test/odrive_speed_test/odrive_speed_test.ino
+++ odrive_speed_test/odrive_speed_test/odrive_speed_test.ino
@@ -1,99 +1,117 @@
-
+#include <SBUS.h>
 #include <SoftwareSerial.h>
 #include <ODriveArduino.h>
 
+#define DEBUG_SERIAL  Serial
+#define SBUS_SERIAL   Serial2
+#define ODRIVE_SERIAL Serial3
+
+#define DEBUG_SERIAL_BAUDRATE 115200
+#define ODRIVE_SERIAL_BAUDRATE 115200
+
 // Printing with stream operator
-template<class T> inline Print& operator <<(Print &obj,     T arg) { obj.print(arg);    return obj; }
-template<>        inline Print& operator <<(Print &obj, float arg) { obj.print(arg, 4); return obj; }
+template<class T> inline Print& operator <<(Print &obj,     T arg) {
+  obj.print(arg);
+  return obj;
+}
+template<>        inline Print& operator <<(Print &obj, float arg) {
+  obj.print(arg, 4);
+  return obj;
+}
 
 // Serial to the ODrive
 //SoftwareSerial odrive_serial(8, 9); //RX (ODrive TX), TX (ODrive RX)
 // Note: you must also connect GND on ODrive to GND on Arduino!
 
 // ODrive object
-ODriveArduino odrive(Serial3);
+ODriveArduino odrive(ODRIVE_SERIAL);
+SBUS x8r(SBUS_SERIAL);
+
+float channels[16];
+bool failSafe;
+bool lostFrame;
+double target_wheel_rpm;
+bool motor1_calibration_finish;
 
 void setup() {
+  motor1_calibration_finish = false;
+  x8r.begin();
   // ODrive uses 115200 baud
-  Serial3.begin(115200);
+  ODRIVE_SERIAL.begin(ODRIVE_SERIAL_BAUDRATE);
 
   // Serial to PC
-  Serial.begin(115200);
-  while (!Serial) ; // wait for Arduino Serial Monitor to open
+  DEBUG_SERIAL.begin(DEBUG_SERIAL_BAUDRATE);
+  while (!DEBUG_SERIAL) ; // wait for Arduino Serial Monitor to open
 
-  Serial.println("ODriveArduino");
-  Serial.println("Setting parameters...");
+  DEBUG_SERIAL.println("ODriveArduino");
+  DEBUG_SERIAL.println("Setting parameters...");
 
   // In this example we set the same parameters to both motors.
   // You can of course set them different if you want.
   // See the documentation or play around in odrivetool to see the available parameters
   for (int axis = 0; axis < 2; ++axis) {
-    Serial3 << "w axis" << axis << ".controller.config.vel_limit " << 22000.0f << '\n';
-    Serial3 << "w axis" << axis << ".motor.config.current_lim " << 11.0f << '\n';
+    ODRIVE_SERIAL << "w axis" << axis << ".controller.config.vel_limit " << 22000.0f << '\n';
+    ODRIVE_SERIAL << "w axis" << axis << ".motor.config.current_lim " << 11.0f << '\n';
     // This ends up writing something like "w axis0.motor.config.current_lim 10.0\n"
   }
 
-  Serial.println("Ready!");
-  Serial.println("Send the character '0' or '1' to calibrate respective motor (you must do this before you can command movement)");
-  Serial.println("Send the character 's' to stop");
-  Serial.println("Send the character 'g' to go");
-  Serial.println("Send the character 'b' to read bus voltage");
-  Serial.println("Send the character 'p' to read motor positions in a 10s loop");
+  DEBUG_SERIAL.println("Ready!");
+  DEBUG_SERIAL.println("Send the character '0' or '1' to calibrate respective motor (you must do this before you can command movement)");
+  DEBUG_SERIAL.println("Send the character 's' to stop");
+  DEBUG_SERIAL.println("Send the character 'g' to go");
+  DEBUG_SERIAL.println("Send the character 'b' to read bus voltage");
+  DEBUG_SERIAL.println("Send the character 'p' to read motor positions in a 10s loop");
 }
 
 void loop() {
-
-  if (Serial.available()) {
-    char c = Serial.read();
+  if (x8r.readCal(&channels[0], &failSafe, &lostFrame)) {
+    target_wheel_rpm = (*(channels + 1) * 150) - 4.3;
+    DEBUG_SERIAL << target_wheel_rpm << '\n';
+  }
+  if (motor1_calibration_finish) {
+    DEBUG_SERIAL.println("Executing test move");
+    odrive.SetVelocity(0, target_wheel_rpm);
+    delay(100);
+  }
+  if (DEBUG_SERIAL.available()) {
+    char c = DEBUG_SERIAL.read();
 
     // Run calibration sequence
     if (c == '0' || c == '1') {
-      int motornum = c-'0';
+      int motornum = c - '0';
       int requested_state;
 
       requested_state = ODriveArduino::AXIS_STATE_MOTOR_CALIBRATION;
-      Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
+      DEBUG_SERIAL << "Axis" << c << ": Requesting state " << requested_state << '\n';
       odrive.run_state(motornum, requested_state, true);
 
       requested_state = ODriveArduino::AXIS_STATE_ENCODER_OFFSET_CALIBRATION;
-      Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
+      DEBUG_SERIAL << "Axis" << c << ": Requesting state " << requested_state << '\n';
       odrive.run_state(motornum, requested_state, true);
 
       requested_state = ODriveArduino::AXIS_STATE_CLOSED_LOOP_CONTROL;
-      Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
+      DEBUG_SERIAL << "Axis" << c << ": Requesting state " << requested_state << '\n';
       odrive.run_state(motornum, requested_state, false); // don't wait
+      delay(100);
+      motor1_calibration_finish = true;
     }
 
-    // Sinusoidal test move
-    if (c == 'g') {
-      Serial.println("Executing test move");
-        odrive.SetVelocity(0, 100);
-        delay(5);
-    }
-    
-    // Stop
-    if (c == 's') {
-      Serial.println("Executing test move");
-        odrive.SetVelocity(0, 0);
-        delay(5);
-    }
-    
     // Read bus voltage
     if (c == 'b') {
-      Serial3 << "r vbus_voltage\n";
-      Serial << "Vbus voltage: " << odrive.readFloat() << '\n';
+      ODRIVE_SERIAL << "r vbus_voltage\n";
+      DEBUG_SERIAL << "Vbus voltage: " << odrive.readFloat() << '\n';
     }
 
     // print motor positions in a 10s loop
     if (c == 'p') {
       static const unsigned long duration = 10000;
       unsigned long start = millis();
-      while(millis() - start < duration) {
+      while (millis() - start < duration) {
         for (int motor = 0; motor < 2; ++motor) {
-          Serial3 << "r axis" << motor << ".encoder.pos_estimate\n";
-          Serial << odrive.readFloat() << '\t';
+          ODRIVE_SERIAL << "r axis" << motor << ".encoder.pos_estimate\n";
+          DEBUG_SERIAL << odrive.readFloat() << '\t';
         }
-        Serial << '\n';
+        DEBUG_SERIAL << '\n';
       }
     }
   }
