9303c947432d5970aef50f335c65baadba6706c8
Donghee Han <hdh7485@gmail.com>
2021-04-26 20:22:59 +0900

    Added RotorArm class files

--- flying_car_linear_actuator/flying_car_linear_actuator.ino
+++ flying_car_linear_actuator/flying_car_linear_actuator.ino
@@ -6,6 +6,8 @@
 #define PWM1 11
 
 #include "SBUS.h"
+#include "RotorArm.h"
+
 SBUS futaba(Serial1);
 uint16_t channels[16];
 bool failSafe;
@@ -13,67 +15,6 @@ bool lostFrame;
 
 int Motor_speed = 100;
 
-class RotorArm {
-  private:
-    int _pin_en1;
-    int _pin_en2;
-    int _pin_pwm;
-    int _pin_fold_sw;
-    int _pin_unfold_sw;
-    int _status;
-  public:
-    RotorArm(int pin_en1 = 9, int pin_en2 = 10, int pin_pwm = 11
-             , int pin_fold_sw = 14, int pin_unfold_sw = 15)
-      : _pin_en1(pin_en1), _pin_en2(pin_en2), _pin_pwm(pin_pwm)
-      , _pin_fold_sw(pin_fold_sw), _pin_unfold_sw(pin_unfold_sw)
-    {
-      pinMode(_pin_en1, OUTPUT);
-      pinMode(_pin_en2, OUTPUT);
-      pinMode(_pin_pwm, OUTPUT);
-      pinMode(_pin_fold_sw, INPUT_PULLUP);
-      pinMode(_pin_unfold_sw, INPUT_PULLUP);
-    }
-    void fold(int pwm = 1024) {
-      if (isFoldSWPushed()) {  // STOP
-        stopMotor();
-      }
-      else {                       // FOLDING
-        digitalWrite(_pin_en1, LOW);
-        digitalWrite(_pin_en2, HIGH);
-        analogWrite(_pin_pwm, pwm);
-      }
-    }
-
-    void unfold(int pwm = 1024) {
-      if (isUnfoldSWPushed()) {  // STOP
-        stopMotor();
-      }
-      else {                       // UNFOLDING
-        digitalWrite(_pin_en1, HIGH);
-        digitalWrite(_pin_en2, LOW);
-        analogWrite(_pin_pwm, pwm);
-      }
-    }
-
-    void stopMotor(int type = 0) {
-      if (type) {
-        digitalWrite(_pin_en1, LOW);
-        digitalWrite(_pin_en2, HIGH);
-        analogWrite(_pin_pwm, 0);
-      }
-      else {
-        digitalWrite(_pin_en1, LOW);
-        digitalWrite(_pin_en2, LOW);
-        analogWrite(_pin_pwm, 1024);
-      }
-    }
-    bool isFoldSWPushed() {
-      return !digitalRead(_pin_fold_sw);
-    }
-    bool isUnfoldSWPushed() {
-      return !digitalRead(_pin_unfold_sw);
-    }
-};
 
 RotorArm arm_FR(11, 12, 2,  14, 24);
 RotorArm arm_RR(26, 25, 5,  14, 27);
